c3a3fa33fb62d6a4fd2721a3f1ca49165fa4d1a7
cheol-95 <rkdcjf0122@gmail.com>
2021-06-01 17:10:35 +0900

    Quest 03-1 별찍기

---
+++ Quest03/skeleton/printStars.js
@@ -0,0 +1,14 @@
+const N = Number(prompt());
+
+let space = ' '.repeat(N);
+let stars = '*';
+
+for (let i=0; i<N; i++){
+    space = space.slice(0, N-i-1);
+    console.log(space + stars)
+    stars += '**';
+}
+
+// for (let i = 0; i < N ; i++){
+//     console.log(' '.repeat(N-i-1) + '*'.repeat(i*2+1))
+// }
