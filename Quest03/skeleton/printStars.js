75e33a2631c8818d8a715a1692e0f6adc509536c
cheol-95 <rkdcjf0122@gmail.com>
2021-06-02 15:54:41 +0900

    printStar 코드 분리

--- Quest03/skeleton/printStars.js
+++ Quest03/skeleton/printStars.js
@@ -1,18 +1,32 @@
+// const star = () => {
+//     const N = Number(prompt());
+
+//     let space = ' '.repeat(N);
+//     let stars = '*';
+    
+//     for (let i=0; i<N; i++){
+//         space = space.slice(0, N-i-1);
+//         console.log(space + stars)
+//         stars += '**';
+//     }
+// }
+
+// star();
+
+const repeatString = (char, count) => {
+    return char.repeat(count)
+}
+
+const printLine = (N, current) => {
+    return repeatString(' ' ,N-current-1) + repeatString('*' ,current*2+1);
+}
+
 const star = () => {
     const N = Number(prompt());
 
-    let space = ' '.repeat(N);
-    let stars = '*';
-    
     for (let i=0; i<N; i++){
-        space = space.slice(0, N-i-1);
-        console.log(space + stars)
-        stars += '**';
+        console.log(printLine(N, i));
     }
 }
 
-star();
-
-// for (let i = 0; i < N ; i++){
-//     console.log(' '.repeat(N-i-1) + '*'.repeat(i*2+1))
-// }
+star()
