eafa003ec50fff787e70f6b5141faac3558f054f
cheol-95 <rkdcjf0122@gmail.com>
2021-06-02 11:07:16 +0900

    별찍기 함수화

--- Quest03/skeleton/printStars.js
+++ Quest03/skeleton/printStars.js
@@ -1,13 +1,17 @@
-const N = Number(prompt());
+const star = () => {
+    const N = Number(prompt());
 
-let space = ' '.repeat(N);
-let stars = '*';
-
-for (let i=0; i<N; i++){
-    space = space.slice(0, N-i-1);
-    console.log(space + stars)
-    stars += '**';
+    let space = ' '.repeat(N);
+    let stars = '*';
+    
+    for (let i=0; i<N; i++){
+        space = space.slice(0, N-i-1);
+        console.log(space + stars)
+        stars += '**';
+    }
 }
+
+star();
 
 // for (let i = 0; i < N ; i++){
 //     console.log(' '.repeat(N-i-1) + '*'.repeat(i*2+1))
