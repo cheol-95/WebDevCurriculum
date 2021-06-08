cdfe1ccb1b5707be9a43b4b9302cbf5f624a535f
cheol-95 <rkdcjf0122@gmail.com>
2021-06-08 17:50:14 +0900

    합성 적용

--- Quest04/skeleton/composition/desktop.js
+++ Quest04/skeleton/composition/desktop.js
@@ -6,52 +6,55 @@ class MakeElement {
   }
 }
 
-// 드래그 이벤트
-const dragAndDrop = (e) => {
-  e.target.style.zIndex = 1000;
-  const move = (pageX, pageY) => {
-    e.target.style.left = pageX - e.offsetX + 'px';
-    e.target.style.top = pageY - e.offsetY + 'px';
-  };
-
-  const onMouseMove = (e) => {
-    move(e.pageX, e.pageY);
-  };
-
-  move(e.pageX, e.pageY);
-  document.addEventListener('mousemove', onMouseMove);
-  e.target.onmouseup = function () {
-    document.removeEventListener('mousemove', onMouseMove);
-    e.target.onmouseup = null;
-  };
-};
-
-// 드래그가 가능한 element
-class DragElement extends MakeElement {
-  constructor(element) {
-    super(element);
-    this.onmousedown = dragAndDrop;
-    this.ondragstart = () => {
-      return false;
-    };
+// 드래그 이벤트를 내포한 클래스
+class DragElement {
+  constructor() {
+    this.onmousedown = this.#dragAndDrop;
+    this.ondragstart = () => false;
   }
+
+  #dragAndDrop = (e) => {
+    e.target.style.zIndex = 1000;
+
+    const move = (pageX, pageY) => {
+      e.target.style.left = pageX - e.offsetX + 'px';
+      e.target.style.top = pageY - e.offsetY + 'px';
+    };
+
+    const onMouseMove = (e) => {
+      move(e.pageX, e.pageY);
+    };
+
+    const onMouseUp = () => {
+      document.removeEventListener('mousemove', onMouseMove);
+      e.target.onmouseup = null;
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    e.target.addEventListener('mouseup', onMouseUp);
+  };
 }
 
 // 아이콘
-class Icon extends DragElement {
+class Icon extends MakeElement {
   #id;
   #src;
   constructor(id, imageSrc) {
     super('img');
     this.#id = id;
     this.#src = imageSrc;
-
     this.className = 'icon';
     this.style.top += size.icon.top + this.#id * size.icon.height + 'px';
 
     this.setAttribute('id', 'icon_' + this.#id);
     this.setAttribute('src', this.#src);
+
+    this.#composition();
   }
+
+  #composition = () => {
+    Object.assign(this, new DragElement());
+  };
 }
 
 // 폴더 창 끄기버튼
@@ -61,18 +64,31 @@ class XBox extends MakeElement {
   constructor(id) {
     super('img');
     this.#id = id;
-
     this.className = 'xbox';
     this.src = this.#src;
 
-    this.onclick = (e) => {
-      Folder.deleteWindow(this.#id);
-    };
+    this.#addEvent();
+    this.#composition();
   }
+
+  #addEvent = () => {
+    this.addEventListener('click', this.#closeWindow);
+  };
+
+  #closeWindow = () => {
+    const forder = document.getElementById('forder_' + this.#id);
+    const window = document.getElementById('window_' + this.#id);
+    forder.hasWindow = false;
+    window.remove();
+  };
+
+  #composition = () => {
+    Object.assign(this, new DragElement());
+  };
 }
 
 // 폴더 창
-class Window extends DragElement {
+class Window extends MakeElement {
   #id;
   #desktopId;
   constructor(desktopId, id) {
@@ -84,6 +100,7 @@ class Window extends DragElement {
     this.setAttribute('id', 'window_' + this.#id);
 
     this.#appendElements();
+    this.#composition();
   }
 
   #appendElements = () => {
@@ -92,7 +109,8 @@ class Window extends DragElement {
   };
 
   #createXbox = () => {
-    this.append(new XBox(this.#id));
+    const xBox = new XBox(this.#id);
+    this.append(xBox);
   };
 
   #createTextbox = () => {
@@ -101,40 +119,56 @@ class Window extends DragElement {
     textbox.innerHTML = `Tab: ${this.#desktopId}<br>Forder: ${this.#id}`;
     this.append(textbox);
   };
+
+  #composition = () => {
+    Object.assign(this, new DragElement());
+  };
 }
 
 // 폴더
 class Folder extends MakeElement {
   #id;
   #src;
-  #hasWindow;
+  #desktopId;
+  hasWindow;
   constructor(desktopId, id, src) {
     super('img');
     this.#id = id;
     this.#src = src;
-
+    this.#desktopId = desktopId;
     this.className = 'forder';
     this.style.top += size.icon.top + this.#id * size.icon.height + 'px';
 
     this.setAttribute('id', 'forder_' + this.#id);
     this.setAttribute('src', this.#src);
 
-    this.ondblclick = () => {
-      if (!this.#hasWindow) {
-        const newWindow = new Window(desktopId, this.#id);
-        this.parentElement.append(newWindow);
-        this.#hasWindow = true;
-      }
-    };
+    this.#addEvent();
+    this.#composition();
   }
 
-  static deleteWindow(id) {
+  #composition = () => {
+    Object.assign(this, new DragElement());
+  };
+
+  #addEvent = () => {
+    this.addEventListener('dblclick', this.#openWindow);
+  };
+
+  #openWindow = () => {
+    if (!this.hasWindow) {
+      const newWindow = new Window(this.#desktopId, this.#id);
+      this.parentElement.append(newWindow);
+      this.hasWindow = true;
+    }
+  };
+
+  static deleteWindow = (id) => {
     const targetWindow = document.getElementById('window_' + id);
     targetWindow.remove();
 
     const targetForder = document.getElementById('forder_' + id);
-    targetForder.#hasWindow = false;
-  }
+    targetForder.hasWindow = false;
+  };
 }
 
 // 상단 탭
@@ -143,20 +177,25 @@ class Tab extends MakeElement {
   constructor(id) {
     super('li');
     this.#id = id;
-
     this.className = 'tab';
     this.innerHTML = this.#id;
 
     this.setAttribute('id', 'tab_' + this.#id);
 
-    this.onclick = (e) => {
-      if (this.#id !== dummy.currentTab) {
-        document.getElementById('desktop_' + this.#id).selected(true);
-        document.getElementById('desktop_' + dummy.currentTab).selected(false);
-        dummy.currentTab = this.#id;
-      }
-    };
+    this.#addEvent();
   }
+
+  #addEvent = () => {
+    this.addEventListener('click', this.#viewingDesktop);
+  };
+
+  #viewingDesktop = () => {
+    if (this.#id !== dummy.currentTab) {
+      document.getElementById('desktop_' + this.#id).selected(true);
+      document.getElementById('desktop_' + dummy.currentTab).selected(false);
+      dummy.currentTab = this.#id;
+    }
+  };
 }
 
 // 바탕화면
