46f2afa29525d986f06b659925cac1ad11b6ef23
cheol-95 <rkdcjf0122@gmail.com>
2021-06-09 22:28:45 +0900

    레이아웃, 리사이징 이벤트 추가

--- Quest05/skeleton/notepad.js
+++ Quest05/skeleton/notepad.js
@@ -1,3 +1,30 @@
-class Notepad {
-	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
+// 뷰 리사이징 이벤트
+const init = () => {
+  (() => {
+    const setViewSize = () => {
+      const layout = document.getElementsByClassName('layout')[0];
+      layout.style.height = window.innerHeight + 'px';
+      layout.style.width = window.innerWidth + 'px';
+    };
+
+    setViewSize();
+    window.addEventListener('resize', setViewSize);
+  })();
 };
+
+class Notepad {
+  constructor() {}
+}
+
+class Tab {
+  #id;
+  constructor(id) {}
+}
+
+class File {
+  #id;
+  #name;
+  #text;
+  #currentPoint; // 스택의 가장 최고 점 참조
+  constructor(id, name) {}
+}
