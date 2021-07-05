83fa4c29f60d3c8c41ae8a9188014afc6f6fcb60
cheol-95 <rkdcjf0122@gmail.com>
2021-07-05 14:22:10 +0900

    server - 파일 유무 로직 controller -> service

--- Quest09/server/controller/editor.mjs
+++ Quest09/server/controller/editor.mjs
@@ -1,58 +1,51 @@
 import * as editService from '../service/editor.mjs';
-// 유효성검사 등
 
 export const getFileList = async (req, res) => {
+  // validation
   const fileList = await editService.getFileList();
-
-  const result = {
+  return res.status(200).json({
     fileList,
-  };
-  return res.status(200).json(result);
+  });
 };
 
 export const getFile = async (req, res) => {
   const { fileName } = req.params;
-
   const data = await editService.getFile(fileName);
 
-  const result = {
+  return res.status(200).json({
     fileName,
     data,
-  };
-  return res.status(200).json(result);
+  });
 };
 
 export const createFile = async (req, res) => {
-  // 존재하는 파일인지 확인
   const { newFileName } = req.body;
 
-  await editService.upsertFile(newFileName, '');
+  await editService.createFile(newFileName, '');
   return res.status(201).end();
 };
 
-export const updateFile = async (req, res) => {
+export const saveFile = async (req, res) => {
   const { fileName } = req.params;
   const { data } = req.body;
 
-  await editService.upsertFile(fileName, data);
-
+  await editService.saveFile(fileName, data);
   return res.status(200).end();
 };
 
 export const saveAsFile = async (req, res) => {
-  // 존재하는 파일인지 확인
   const { fileName: oldFileName } = req.params;
   const { newFileName, data } = req.body;
 
   await editService.renameFile(oldFileName, newFileName);
-  await editService.upsertFile(newFileName, data);
+  await editService.saveFile(newFileName, data);
 
-  return res.status(201).end();
+  return res.status(200).end();
 };
 
 export const deleteFile = async (req, res) => {
   const { fileName: deleteFileName } = req.params;
-  await editService.deleteFile(deleteFileName);
 
+  await editService.deleteFile(deleteFileName);
   return res.status(200).end();
 };
--- Quest09/server/service/editor.mjs
+++ Quest09/server/service/editor.mjs
@@ -1,34 +1,45 @@
-import fs from 'fs/promises';
+import { promises as fsp, existsSync } from 'fs';
+import config from '../config/config.mjs';
 
-const FILEPATH = './public/';
+const { FILEPATH } = config;
+
+const isExist = (fileName) => {
+  if (existsSync(makeFilePath(fileName))) {
+    throw { code: 'EXISTS' };
+  }
+};
 
 const makeFilePath = (fileName) => {
   return FILEPATH + fileName + '.txt';
 };
 
 export const getFileList = async () => {
-  const fileList = await fs.readdir(FILEPATH);
+  const fileList = await fsp.readdir(FILEPATH);
   return fileList.map((fileName) => {
     return fileName.split('.')[0];
   });
 };
 
 export const getFile = async (fileName) => {
-  const data = await fs.readFile(makeFilePath(fileName));
+  const data = await fsp.readFile(makeFilePath(fileName));
   return data.toString();
 };
 
-export const upsertFile = async (fileName, data) => {
-  await fs.writeFile(makeFilePath(fileName), data);
+export const createFile = async (fileName) => {
+  isExist(fileName);
+  await fsp.writeFile(makeFilePath(fileName), '');
+};
+
+export const saveFile = async (fileName, data) => {
+  await fsp.writeFile(makeFilePath(fileName), data);
 };
 
 export const renameFile = async (oldFileName, newFileName) => {
-  // DB값 수정 부
   const oldFile = makeFilePath(oldFileName);
   const newFile = makeFilePath(newFileName);
-  await fs.rename(oldFile, newFile);
+  await fsp.rename(oldFile, newFile); // DB의 파일명만 변경할 예정
 };
 
 export const deleteFile = async (deleteFileName) => {
-  await fs.unlink(makeFilePath(deleteFileName));
+  await fsp.unlink(makeFilePath(deleteFileName));
 };
