47c0a49a9e7918b2532a0ef5ea74585766a71215
cheol-95 <rkdcjf0122@gmail.com>
2021-08-03 17:13:36 +0900

    ts-jest 제거

--- packages/server/jest.config.mjs
+++ packages/server/jest.config.mjs
@@ -1,13 +1,7 @@
 export default {
-  testRegex: '\\.test\\.js$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   globals: {
-    'ts-jest': {
-      diagnostics: true,
-    },
     graphqlPath: '/graphql',
   },
 
   verbose: true,
-  // testMatch: ['test/*.test.js'],
 };
