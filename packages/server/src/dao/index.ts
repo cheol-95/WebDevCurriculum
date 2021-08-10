de00a4fed7948434051e928d7bd8b89b549b5774
cheol-95 <rkdcjf0122@gmail.com>
2021-08-10 16:19:45 +0900

    미사용 로직 제거

--- packages/server/src/dao/index.ts
+++ packages/server/src/dao/index.ts
@@ -1,7 +1,7 @@
 import User from './user';
 import File from './file';
 
-import { getDigest } from '../lib/auth';
+// import { getDigest } from '../lib/auth';
 import sequelize from './sequelize';
 
 User.hasMany(File, {
@@ -19,20 +19,20 @@ export { User, File };
 (async () => {
   await sequelize.sync({});
 
-  const isData = await User.findOne({ where: { email: 'test1' } });
-  if (!isData) {
-    try {
-      ['test1', 'test2', 'test3'].forEach(async (email) => {
-        const { salt, digest: password } = await getDigest(email, null);
-        await User.create({
-          email,
-          password,
-          salt,
-        });
-      });
-    } catch (err) {
-      /* eslint-disable-next-line no-console */
-      console.log('err: ', err);
-    }
-  }
+  //   const isData = await User.findOne({ where: { email: 'test1' } });
+  //   if (!isData) {
+  //     try {
+  //       ['test1', 'test2', 'test3'].forEach(async (email) => {
+  //         const { salt, digest: password } = await getDigest(email, null);
+  //         await User.create({
+  //           email,
+  //           password,
+  //           salt,
+  //         });
+  //       });
+  //     } catch (err) {
+  //       /* eslint-disable-next-line no-console */
+  //       console.log('err: ', err);
+  //     }
+  //   }
 })();
